ccaeb09c5bc25754f577119877afedded8f6a1ba
JonahWang <jonah_wang@outlook.com>
2016-06-30 12:18:48 +0800

    login success

--- jonah/blog/views.py
+++ jonah/blog/views.py
@@ -6,3 +6,24 @@ def index(request):
     context['name'] = Constant.BLOG_NAME
     context['desc'] = Constant.BLOG_DESC
     return render(request,'index.html',context)
+
+def login(request):
+    if request.method == "POST":
+        if 'username' in request.POST and 'password' in request.POST:
+            un = request.POST['username']
+            pw = request.POST['password']
+            '''校验账号密码'''
+            if un == "jonah" and pw == '5442377':
+                #save session
+                request.session['username'] = un
+                request.session['password'] = pw
+                context = {}
+                context['name'] = Constant.BLOG_NAME
+                context['desc'] = Constant.BLOG_DESC
+                context['username'] = un
+                return render(request,'index.html',context)
+        else:
+            context = {}
+            context['message'] = '账号或密码错误'
+            return  render(request,'login.html',context)
+    return render(request,'login.html')
--- jonah/jonah/urls.py
+++ jonah/jonah/urls.py
@@ -19,5 +19,6 @@ from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',views.index)
+    url(r'^$',views.index),
+    url(r'^login$',views.login)
 ]
